0e8a167e6d932bfafb970a6b892e62e776718e9b
CaptainKurt <walkkg12@wfu.edu>
2015-02-28 19:55:36 -0500

    Initial Commit

---
+++ Hoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hoop
+//
+//  Created by Kurt Walker on 2/28/15.
+//  Copyright (c) 2015 Hoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
